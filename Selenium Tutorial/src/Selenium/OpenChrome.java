d4bf16d57ffa4c3cbf4585b61fe26fb3f39c1040
vinot <vinot@LAPTOP-F75NTUBU>
2021-07-27 13:54:25 +0530

    initial commit

---
+++ Selenium Tutorial/src/Selenium/OpenChrome.java
@@ -0,0 +1,31 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenChrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1.OpenChrome
+		//2.google home page
+	// choosing the driver and providing the location
+	System.setProperty("webdriver.chrome.driver", "C:\\Vinoth\\Software\\chromedriver_win32\\chromedriver.exe");
+	// To open the Chrome
+	WebDriver driver = new ChromeDriver();
+	// To open a link in Chrome.
+	driver.get("http://www.google.co.in");
+	/*to choose the Firfox
+	webdriver.gecko.driver
+	WebDriver driver = new FirefoxDriver();*/
+	//1.enter a search term
+	//2.click the wikipedia link
+	driver.findElement(By.name("q")).sendKeys("Agni"+Keys.ENTER);
+		
+	
+	//driver.quit();
+	}
+
+}
